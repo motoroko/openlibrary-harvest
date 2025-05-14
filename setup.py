6f9cdc8f394addf3d91d7562f6ccfa8e69683944
Rauf <blograuf.kr@gmail.com>
2025-05-14 14:57:39 +0700

    Fix structure for pip

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='openlibrary_harvest',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(where="openlibrary_harvest"),  # Tambahkan ini
     install_requires=[
-        'tqdm'
+        'tqdm',
     ],
     include_package_data=True,
-    package_data={'': ['../scrape_openlibrary.js']},
-    author='Khalifatur Rauf',
-    author_email='blograuf.kr@gmail.com',
+    package_data={'': ['scrape_openlibrary.js']},  # Pastikan file js dimasukkan
+    author='Your Name',
+    author_email='your@email.com',
     description='Scrape genres from OpenLibrary using Playwright via Node.js',
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/openlibrary-harvest',
+    url='https://github.com/motoroko/openlibrary-harvest',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',

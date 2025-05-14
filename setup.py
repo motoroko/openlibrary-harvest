e0164072e465552081c409083ab06f4cee7ee424
Rauf <blograuf.kr@gmail.com>
2025-05-14 15:01:00 +0700

    Fix setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='openlibrary_harvest',
     version='0.1.0',
-    packages=find_packages(where="openlibrary_harvest"),  # Tambahkan ini
+    packages=find_packages(where="openlibrary_harvest"),  # Menggunakan find_packages() untuk mencari modul
     install_requires=[
         'tqdm',
     ],
     include_package_data=True,
-    package_data={'': ['scrape_openlibrary.js']},  # Pastikan file js dimasukkan
-    author='Your Name',
-    author_email='your@email.com',
+    package_data={'': ['scrape_openlibrary.js']},  # Pastikan file js dimasukkan jika diperlukan
+    author='Khalifatur Rauf',
+    author_email='blograuf.kr@gmail.com',
     description='Scrape genres from OpenLibrary using Playwright via Node.js',
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
